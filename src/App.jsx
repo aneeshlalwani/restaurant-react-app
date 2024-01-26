cfcc752ed9bf24bd53e1258be1b518fb5a38bd7a
Aneesh <aneeshlalwani.bscssef20@iba-suk.edu.pk>
2024-01-27 00:09:38 +0500

    menu added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import './assets/styles/styles.css';
 import Header from "./components/Header/Header"
 import HeroSection from './components/HeroSection/HeroSection';
+import Menu from './components/Menu/Menu';
 
 function App() {
   return (
     <>
       <Header />
       <HeroSection />
-    </>
+      <Menu />   
+       </>
   )
 }
 
